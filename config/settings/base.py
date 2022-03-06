208527790eb7060fe72ddcd6b0113d31ee7cdfd0
c0rat1e5 <cccc0rat1e5@icloud.com>
2022-03-06 14:42:46 +0900

    product model

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,6 +85,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "main.users",
+    "main.app1",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
